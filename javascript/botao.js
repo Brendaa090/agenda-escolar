016daa23c72a03d79816ea509cf3ce50e5da9137
BpFoxy <145776129+Brendaa090@users.noreply.github.com>
2025-05-02 22:20:31 -0300

    Update botao.js

--- javascript/botao.js
+++ javascript/botao.js
@@ -1,4 +1,4 @@
-// Exemplo de funcionalidade para os botões
+
 document.querySelectorAll("button").forEach(button => {
     button.addEventListener("click", () => {
       alert(`Você clicou no botão: ${button.textContent}`);
